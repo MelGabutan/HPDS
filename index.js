429c11c017694f4662a7386c35b6e6de14c1ae33
Mel Jefferson T. Gabutan <70356954+MelGabutan@users.noreply.github.com>
2020-10-12 22:06:38 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function moveUsername() {
+	username = document.getElementById("username-title");
+
+	username.style.transform = "translate(-9px, -33px)";
+	username.style.color = "#3AAFA9";
+	username.style.fontSize = "14px";
+
+}
+
+function movePassword() {
+	password = document.getElementById("password-title");
+
+	password.style.transform = "translate(-9px, -33px)";
+	password.style.color = "#3AAFA9";
+	password.style.fontSize = "14px";
+}
